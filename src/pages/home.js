a70b41aebcbb03cb56e0c0fa323bf4716a06c308
philiponions <hueycoy9@gmail.com>
2023-04-20 00:29:14 -0600

    add target=_blank for an href

--- src/pages/home.js
+++ src/pages/home.js
@@ -148,7 +148,7 @@ export default function Home() {
                             <div className={raleway.className}>
                                 <Section>
                                     <div class="lg:flex sm:items-align justify-between p-16 w-full items-center">
-                                        <a href="https://thepowdermill.ca/"><img src="../images/pmlogo.png"></img></a>
+                                        <a target="_blank" href="https://thepowdermill.ca/"><img src="../images/pmlogo.png"></img></a>
                                         <div class="w-full ml-32">
                                             <h1 class="text-1.5xl font-black">Software Developer Intern</h1>
                                             <div class="flex justify-between">
