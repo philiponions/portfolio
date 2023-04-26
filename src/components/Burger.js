c5c36ae312304aee1176e06f2100335cd298b86c
philiponions <hueycoy9@gmail.com>
2023-04-26 01:01:01 -0600

    Add burger component

---
+++ src/components/Burger.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Burger() {
+  return (
+    <div>
+        <div>item</div>
+        <div>item</div>
+        <div>item</div>
+    </div>
+  )
+}
+
+export default Burger
