6b3848b89ee676399eeee213e75c887c7b15a630
philiponions <hueycoy9@gmail.com>
2023-04-25 23:43:26 -0600

    Add mobile size support

--- src/pages/home.js
+++ src/pages/home.js
@@ -27,8 +27,8 @@ export default function Home() {
   return (         
       <div>        
         <div className={poppins.className}>
-          <div class="md:flex items-center justify-center p-16">
-            <div class="w-1/2 flex-col">
+          <div class="flex flex-col-reverse md:flex-row items-center justify-center p-16 gap-32 md:gap-0">
+            <div class="lg:w-1/2 lg:center-left text-center md:text-left">
               <AnimatedTitle text={"Hey There!"}/>     
               <motion.span className="inline-block" initial={{ opacity: 0, y: "0.25em" }} animate={{ opacity: 1, y: "0em", transition: { duration: 1, ease: [0.2, 0.65, 0.3, 0.9] } }}>    
                 <h3 class="text-3xl">I'm Huey Gonzales, </h3>
@@ -41,7 +41,7 @@ export default function Home() {
                 <h3 class="text-3xl font-extrabold">Software Developer</h3> 
               </motion.span>                                 
             </div>
-            <div class="ml-32">
+            <div class="md:ml-32 ml-0">
             <motion.div
                 className="box"
                 initial={{ opacity: 0, scale: 0.5 }}
@@ -62,7 +62,7 @@ export default function Home() {
                 </Section>
                 <div>
                     <Section>
-                        <div class="shadow-lg p-8">
+                        <div class="shadow-lg p-8 md:text-left text-center">
                             <h1 class="text-2xl font-bold">
                             Hi there. It's Huey 😃
                             </h1>
@@ -75,27 +75,27 @@ export default function Home() {
                         </div>
                     </Section>                    
                     <Section>
-                        <div class="shadow-lg p-8">
+                        <div class="shadow-lg p-8 md:text-left text-center">
                             <h1 class="text-2xl font-bold">
                             Programming Languages
                             </h1>
-                            <div>
+                            <div class="">
                                 <p className={raleway.className}>
                                 I'm quite proficient in the following:
                                 </p>                                                        
-                                    <div class="flex gap-2 my-4">
-                                        <img src="https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white"/>
-                                        <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"/>
-                                        <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"/>
-                                        <img src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white"/>
-                                        <img src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54"/>
-                                        <img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white"/>
-                                    </div>                            
+                                <div class="flex gap-2 my-4 flex-wrap justify-center md:justify-start">
+                                    <img src="https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white"/>
+                                    <img src="https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white"/>
+                                    <img src="https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E"/>
+                                    <img src="https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=openjdk&logoColor=white"/>
+                                    <img src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54"/>
+                                    <img src="https://img.shields.io/badge/c%23-%23239120.svg?style=for-the-badge&logo=c-sharp&logoColor=white"/>
+                                </div>                            
                             </div>
                         </div>
                     </Section>
                     <Section>
-                        <div class="shadow-lg p-8 my-10">
+                        <div class="shadow-lg p-8 my-10 md:text-left text-center">
                             <h1 class="text-2xl font-bold">
                             Tech Stacks
                             </h1>
@@ -123,16 +123,16 @@ export default function Home() {
                     </Section>
                     <Section>                        
                         <div class="shadow-lg pt-8 px-8 my-10">
-                            <h1 class="text-2xl font-bold">
+                            <h1 class="text-2xl font-bold text-center md:text-start">
                             Work Experience
                             </h1>
                             <div className={raleway.className}> 
                                 <Section>                            
-                                    <div class="lg:flex sm:items-align justify-between p-16 w-full items-center">
+                                    <div class="flex md:flex-row flex-col justify-between p-0 md:p-16 w-full items-center">
                                         <a href="https://projecthumancity.com/" target="_blank"><img src="../images/phclogo.png"></img></a>
-                                        <div class="w-full ml-32">
-                                            <h1 class="text-1.5xl font-black">Software Developer Intern</h1>
-                                            <div class="flex justify-between">
+                                        <div class="w-full ml-0 md:ml-32">
+                                            <h1 class="text-1.5xl font-black md:text-left text-center">Software Developer Intern</h1>
+                                            <div class="flex md:justify-between justify-center lg:flex-row flex-col md:items-start items-center">
                                                 <h2 class="text-xl font-black">Project: Human City</h2>
                                                 <h2 class="text-xl font-black italic">Jan 2023-Apr 2023</h2>
                                             </div>
@@ -147,11 +147,11 @@ export default function Home() {
                             </div>
                             <div className={raleway.className}>
                                 <Section>
-                                    <div class="lg:flex sm:items-align justify-between p-16 w-full items-center">
+                                    <div class="flex md:flex-row flex-col justify-between p-0 md:p-16 w-full items-center">
                                         <a target="_blank" href="https://thepowdermill.ca/"><img src="../images/pmlogo.png"></img></a>
-                                        <div class="w-full ml-32">
-                                            <h1 class="text-1.5xl font-black">Software Developer Intern</h1>
-                                            <div class="flex justify-between">
+                                        <div class="w-full ml-0 md:ml-32">
+                                            <h1 class="text-1.5xl font-black md:text-left text-center">Software Developer Intern</h1>
+                                            <div class="flex md:justify-between justify-center lg:flex-row flex-col md:items-start items-center">
                                                 <h2 class="text-xl font-black">The Powdermill</h2>
                                                 <h2 class="text-xl font-black italic">May 2022-Aug 2022</h2>
                                             </div>
@@ -172,13 +172,13 @@ export default function Home() {
                     <Section>
                         <div id="section-3"/>
                         <h3 class="text-3xl font-extrabold">Projects</h3>  
-                        <div class="flex grid lg:grid-cols-3 sm:grid-cols-3 items-center gap-4"> 
+                        <div class="flex grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center gap-4"> 
                             <a target="_blank" href="https://who-is-teaching.netlify.app/"><Project src="../images/whoisteaching.png" name="Who-is-Teaching?" desc="A website that allows you to view ratings of the professors who are currently teaching at uAlberta"/></a>             
                             <a target="_blank" href="https://www.youtube.com/watch?v=wD61rurNSts"><Project src="../images/qradventure.png" name="QRAdventure" desc="An android game that gamifies the collection of scanning QR Codes!"/></a>       
                             <a target="_blank" href="https://gameslib.onrender.com/swagger/index.html"><Project src="../images/gameslib.png" name="GamesLibrary" desc="A REST API made in .NET Core which connects to a database filled with over 1000+ games"/></a>       
                             <a target="_blank" href="https://github.com/philiponions/MyFriends"><Project src="../images/myfriends.png" name="MyFriends" desc="A mobile application that lets you store details about your friends (like their birthdays!)"/></a>
                             <a target="_blank" href="https://portfolio-eosin-ten.vercel.app/"><Project src="../images/portfolio.png" name="This Portfolio" desc="The portfolio you're looking at right NOW. Made in Next.js with tailwind css.."/></a>                                          
-                            <a target="_blank" href="https://github.com/philiponions/FormuLister-Mobile-App"><Project src="../images/formulister.png" name="FormuLister (Work in progress)" desc="A mobile app that allows you to conveniently reuse math formulas you use everyday."></Project></a>       
+                            <a target="_blank" href="https://github.com/philiponions/FormuLister-Mobile-App"><Project src="../images/formulister.png" name="FormuLister" desc="A mobile app that allows you to conveniently reuse math formulas you use everyday."></Project></a>       
                         </div>
                     <center class="m-5"> Contact Page coming soon! :)</center>
                     </Section>
